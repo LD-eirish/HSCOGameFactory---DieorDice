5cbe50dd7e4118b7930086ed8bd6941955bd09b9
Silverfox22 <kai-gareis@outlook.de>
2024-03-30 11:03:26 +0100

    überarbeitet

--- Bullet Hell neu/player.gd
+++ Bullet Hell neu/player.gd
@@ -5,28 +5,34 @@ class_name PlayerCharacter
 const max_speed = 400
 const accel = 1500 #Beschleunigung
 const friction = 600 #Reibung
+var rotation_speed = 1.5
 
-var input = Vector2.ZERO
+var direction = Vector2.ZERO
+var rotation_direction = 0
 
 func _physics_process(delta):
 	player_movement(delta)
 
 
 func get_input():
-	input.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
-	input.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
-	return input.normalized()
+	#direction.x = int(Input.is_action_pressed("right")) - int(Input.is_action_pressed("left"))
+	#direction.y = int(Input.is_action_pressed("down")) - int(Input.is_action_pressed("up"))
+	direction = Input.get_vector("left", "right", "up", "down") #movement
+	#rotation_direction = Input.get_axis("left", "right") #rotation movement
+	#velocity = transform.x * Input.get_axis("down", "up") * accel
+	return direction.normalized(); rotation_direction.normalized()
 	
 func player_movement(delta):
-	input = get_input()
+	get_input()
 	
-	if input == Vector2.ZERO:
+	if direction == Vector2.ZERO:
 		if velocity.length() > (friction * delta):
 			velocity -= velocity.normalized() * (friction * delta)
 		else:
 			velocity = Vector2.ZERO
 	else:
-		velocity += (input * accel * delta)
+		velocity += (direction * accel * delta)
 		velocity = velocity.limit_length(max_speed)
+		#rotation += rotation_direction * rotation_speed * delta
 		
 	move_and_slide()
