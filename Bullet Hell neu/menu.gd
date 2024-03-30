618b02f33f7dcbb70f328277373033afb0c20f82
Justin Will <eirishofficial@gmail.com>
2024-03-30 12:20:11 +0100

    menu second iteration

--- Bullet Hell neu/menu.gd
+++ Bullet Hell neu/menu.gd
@@ -1,8 +1,18 @@
 extends Control
 
+@onready var start_button = $VBoxContainer/start_button as Button
+@onready var options_button = $VBoxContainer/options_button as Button
+@onready var quit_button = $VBoxContainer/quit_button as Button
+@onready var v_box_container = $VBoxContainer as VBoxContainer
+@onready var options = $Options as OptionsMenu
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/start.grab_focus()
+	$VBoxContainer/start_button.grab_focus()
+	options.exit_options_menu.connect(on_exit_options_menu)
+	v_box_container.visible = true
+	options.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,7 +25,16 @@ func _on_start_pressed():
 
 
 func _on_options_pressed():
-	get_tree().change_scene_to_file("res://scenes/options.tscn") 
-
+	v_box_container.visible = false
+	options.set_process(true)
+	options.visible = true
+	##get_tree().change_scene_to_file("res://scenes/options.tscn") 
+	
 func _on_quit_pressed():
 	get_tree().quit()
+	
+func on_exit_options_menu() -> void:
+	v_box_container.visible = true
+	options.visible = false
+	
+ 
