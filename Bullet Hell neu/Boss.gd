565ad113908fb39e4ecd3372a9dd30e7fdf0d5c8
Silverfox22 <kai-gareis@outlook.de>
2024-03-31 11:16:43 +0200

    short timer

--- Bullet Hell neu/Boss.gd
+++ Bullet Hell neu/Boss.gd
@@ -9,13 +9,20 @@ var player_position = Vector2.ZERO
 #var player = null
 
 signal shoot
+var can_shoot : bool
 #@onready player_position = $"../player".global_position
 
+func _ready():
+	can_shoot = true
+
 func _process(delta):
-	var player_pos = get_parent().get_node("player").global_position
-	var dir = player_pos - position
-	shoot.emit(position, dir)
-	print(player_pos)
+	if can_shoot:
+		var player_pos = get_parent().get_node("player").global_position
+		var dir = player_pos - position
+		shoot.emit(position, dir)
+		print(player_pos)
+		can_shoot = false
+		$ShortTimer.start()
 
 
 
@@ -28,3 +35,7 @@ func _process(delta):
 #func _on_detection_area_body_entered(body):
 	#player = body
 
+
+
+func _on_short_timer_timeout():
+	can_shoot = true
