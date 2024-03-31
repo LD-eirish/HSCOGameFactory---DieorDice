eccb432e96257c75ac4d6499dc7624c11596d5a8
Yu-Soseiki <Jutendo@outlook.de>
2024-03-31 17:53:11 +0200

    SOund Design is done
    At least 3 people get their job done

--- Bullet Hell neu/RealTOT.gd
+++ Bullet Hell neu/RealTOT.gd
@@ -2,6 +2,9 @@ extends Node
 
 var aniCount = 0
 
+func _ready():
+	AudioManager.main_bgm_sfx.stream_paused = false
+
 #func playphase():
 	#aniCount= aniCount +1
 	#if aniCount == 1:
@@ -23,9 +26,13 @@ func _on_ui_custom():
 		AudioManager.dont_think_you_can_escape_sfx.play()
 	elif aniCount == 2:
 		$Label2/Ani2.play("Ani2")
+		AudioManager.this_isnt_even_my_final_form_sfx.play()
 	elif aniCount == 3:
 		$Label3/Ani3.play("Ani3")
+		AudioManager.supposed_to_care_sfx.play()
 	elif aniCount == 4:
 		$Label4/Ani4.play("Ani4")
+		AudioManager.still_not_final_form_sfx.play()
 	elif aniCount == 5:
 		$Label5/Ani5.play("Ani5")
+		AudioManager.anger_will_downfall_sfx.play()
