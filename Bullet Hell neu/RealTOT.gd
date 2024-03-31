9789ad6ebeb61e49a2c59bc20cde181eab6cff7d
Yu-Soseiki <Jutendo@outlook.de>
2024-03-31 11:51:47 +0200

    RealTOT FIx?

--- Bullet Hell neu/RealTOT.gd
+++ Bullet Hell neu/RealTOT.gd
@@ -1,14 +1,5 @@
 extends CanvasLayer
 
-#signal on_transition_finished
-
-#@onready var label = $"."
-#@onready var animationplayer = $AnimationPlayer
-
-#func _ready():
-#	label.visible = false
-#	animationplayer.animation_finished.connect(_on_animation_finished)
-
 var aniCount = 0
 
 func playphase():
@@ -23,16 +14,3 @@ func playphase():
 		$Label/AnimationPlayer.play("Ani4")
 	elif aniCount == 5:
 		$Label/AnimationPlayer.play("Ani5")
-
-
-#func _on_animation_finished(anim_name):
-#	if anim_name == "FADE IN":
-#		on_transition_finished.emit()
-#		animationplayer.play("FADE IN_2 (OUT)")
-#	elif anim_name == "FADE IN_2 (OUT)":
-#		label.visible = false
-
-
-#func phase1to2():
-#	label.visible = true
-#	animationplayer.play("FADE IN")
